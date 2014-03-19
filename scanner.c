344120621d4bbcf2e02baf4e0023dc1fc943e0c5
maxxrdrgz <maxxrdrgz@gmail.com>
2014-03-19 15:45:35 -0600

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -14,15 +14,16 @@
  You need to design the proper parameter list and 
  return types for functions with ???.
  ******************/
-static ??? get_char(???);
-static ??? skip_comment(???);
-static ??? skip_blanks(???);
-static ??? get_word(???);
-static ??? get_number(???);
-static ??? get_string(???);
+static char get_char(char *ch);
+static char skip_comment(char *ch);
+static char skip_blanks(???);
+static char* get_word(???);
+static int get_number(???);
+static char* get_string(???);
 static ??? get_special(???);
 static ??? downshift_word(???);
 static BOOLEAN is_reserved_word(???);
+char array[MAX_SOURCE_LINE_LENGTH];
 
 typedef enum
 {
@@ -92,17 +93,23 @@ Token* get_token()
 {
     char ch; //This can be the current character you are examining during scanning.
     char token_string[MAX_TOKEN_STRING_LENGTH]; //Store your token here as you build it.
-    char *token_ptr = token_string[0]; //write some code to point this to the beginning of token_string
+    char *token_ptr = ???; //write some code to point this to the beginning of token_string
     ???;  //I am missing the most important variable in the function, what is it?  Hint: what should I return?
     
-    skip_blanks(???);//1.  Skip past all of the blanks
+    //1.  Skip past all of the blanks
     //2.  figure out which case you are dealing with LETTER, DIGIT, QUOTE, EOF, or special, by examining ch
     //3.  Call the appropriate function to deal with the cases in 2.
     
     return ???; //What should be returned here?
 }
-static char* get_char()
+static char get_char(char *ch)
 {
+    int i = 0;
+    
+    while(array[i] != '\n')
+    {
+        &ch = fread(array, , 1,src_file);
+    }
     /*
      If at the end of the current line (how do you check for that?),
      we should call get source line.  If at the EOF (end of file) we should
@@ -113,7 +120,7 @@ static char* get_char()
      Write some code to set the character ch to the next character in the buffer
      */
 }
-static ??? skip_blanks(???)
+static char* skip_blanks(???)
 {
     /*
      Write some code to skip past the blanks in the program and return a pointer
@@ -121,14 +128,14 @@ static ??? skip_blanks(???)
      */
     
 }
-static ??? skip_comment(???)
+static char* skip_comment(???)
 {
     /*
      Write some code to skip past the comments in the program and return a pointer
      to the first non blank character.  Watch out for the EOF character.
      */
 }
-static ??? get_word(???)
+static char* get_word(???)
 {
     /*
      Write some code to Extract the word
@@ -160,7 +167,7 @@ static ??? get_special(???)
      some are double-character.  Set the token appropriately.
      */
 }
-static ??? downshift_word(???)
+static char downshift_word(char *ch)
 {
     /*
      Make all of the characters in the incoming word lower case.
